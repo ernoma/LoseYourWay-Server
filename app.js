0df99ea8aef167fc2e0197d838f769ffd7b05ebb
ernoma <ernoma@gmail.com>
2016-03-15 10:31:05 +0200

    Remove app.listen call from app.js

--- app.js
+++ app.js
@@ -69,6 +69,6 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-var port = process.env.PORT || 8080;
-app.listen(port);
-console.log('The magic happens on port ' + port);
+//var port = process.env.PORT || 8080;
+//app.listen(port);
+//console.log('The magic happens on port ' + port);
