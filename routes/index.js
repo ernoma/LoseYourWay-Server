864cc008c3318b1645698ccded7758fd16108855
ernoma <ernoma@gmail.com>
2016-03-16 20:50:48 +0200

    Add sample postcard

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/sample', function(req, res, next) {
+  res.render('sample', { title: 'Sample Postcard' });
+});
+
 module.exports = router;
