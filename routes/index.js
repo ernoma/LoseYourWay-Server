5f3a788b780ce5020068830133afc472eccb182f
ernoma <ernoma@gmail.com>
2016-04-07 11:02:31 +0300

    Add privacy policy

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/sample', function(req, res, next) {
   res.render('sample', { title: 'Sample Postcard' });
 });
 
+router.get('/privacy', function(req, res, next) {
+  res.render('privacy', { title: 'Privacy Policy' });
+});
+
 module.exports = router;
