efa3f1ce9215e304d2df27dcb5ffd0afe1009605
ernoma <ernoma@gmail.com>
2016-04-25 21:52:15 +0300

    Cache of the hotosm projects daily for faster response

--- hotosm/hotosm.js
+++ hotosm/hotosm.js
@@ -3,6 +3,23 @@ var request = require('request');
 
 var projectsURL = 'https://tasks.hotosm.org/projects.json';
 
+var projects = {};
+
+function updateProjects() {
+    request(projectsURL, function(error, response, body) {
+	if (!error && response.statusCode == 200) {
+	    projects = JSON.parse(body);
+	} else {
+	    // Nothing to do
+	}
+    });
+}
+
+updateProjects();
+setInterval(function() {
+    updateProjects();
+}, 3600 * 24 * 1000); // Daily
+
 exports.getProjects = function getProjects(req, res, next) {
-    request(projectsURL).pipe(res);
+    res.json(projects);
 }
